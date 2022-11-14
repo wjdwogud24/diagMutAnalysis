38bcab0196db5377a3f82d8b85481648d93bba8d
unknown <jungja16@mail.utoronto.ca>
2022-11-14 15:58:32 -0500

    added description and examples to data and functions

--- R/mutationAnalysis.R
+++ R/mutationAnalysis.R
@@ -1,10 +1,48 @@
-#' Description about this mutation
+#' Get count and percentage of gene's affected by mutation
 #'
+#' A function that calculates the percentage of each observed mutation's
+#' frequency in the data given a data frame with column's icgc_donor_id,
+#' and gene_affected.
 #'
+#' @param mutationData A data frame with column's icgc_donor_id,
+#' and gene affected. The data frame should be in the same format as the icgc
+#' database's DCC Data Release.
+#'
+#' @return Returns a data frame with results
+#' \itemize{
+#' \item gene_affected - Gene code for designated mutated gene in donors
+#' \item mutation_count - The number of mutation's observed in sample for
+#' gene_affected
+#' \item percentage_of_sample_with_mutated_gene - The percentage of the
+#' frequency of observed mutated gene in sample
+#' }
+#'
+#' @examples
+#' # Example 1:
+#' # Using icgc_data available inside package
+#'
+#' # Get gene's mutation frequency, and percentage in sample
+#' mutationPercentageResults <- mutationPercentage(icgc_data)
+#' mutationPercentageResults
+#'
+#' @references
+#' Hadley Wickham, Romain Francois, Lionel Henry and Kirill Muller
+#' (2022). dplyr: A Grammar of Data Manipulation. R package version
+#' 1.0.10. https://CRAN.R-project.org/package=dplyr
+#'
+#' @export
+#' @import dplyr
+#' @importFrom dplyr distinct
+#' @importFrom dplyr add_count
+#' @importFrom dplyr mutate
 
 mutationPercentage <- function(mutationData){
 
-  inter <- dplyr::distinct(mutationData,mutationData$icgc_donor_id, mutationData$gene_affected, .keep_all = TRUE)
+  #Performing checks on user input
+
+
+  inter <- dplyr::distinct(mutationData,mutationData$icgc_donor_id,
+                           mutationData$gene_affected, .keep_all = TRUE)
   inter <- inter[,-c(length(inter),length(inter) - 1)]
 
 
@@ -18,33 +56,89 @@ mutationPercentage <- function(mutationData){
 
   inter <- dplyr::distinct(inter, inter$gene_affected, .keep_all = TRUE)
 
-  #percentage_of_sample_with_gene is the count of a specific gene mutated/number of donors
-  inter <- dplyr::mutate(inter, percentage_of_sample_with_mutated_gene = (mutation_count/number_of_sample))
+  #percentage_of_sample_with_gene is the count
+  #of a specific gene mutated/number of donors
+  inter <- dplyr::mutate(inter, percentage_of_sample_with_mutated_gene =
+                           (mutation_count/number_of_sample))
 
   #Set number of sample as a dataframe before inserting into result
   number_sample = data.frame(number_of_sample)
   colnames(number_sample) <- "number_of_sample"
 
 
-  result <- c(inter[,c("gene_affected","mutation_count", "percentage_of_sample_with_mutated_gene")], number_sample)
+  result <- inter[,c("gene_affected","mutation_count",
+                     "percentage_of_sample_with_mutated_gene")]
   return(result)
 }
 
 
 
 
+#' Bar plot and pie chart for mutation type data samples
+#'
+#' A function that returns a list containing a list of plots for mutation type
+#' data samples
+#'
+#' @param mutationData A data frame with column's icgc_mutation_id,
+#' cds_mutation, and consequence_type. The data frame should be in the same
+#' format as the icgc database's DCC data release
+#'
+#' @return Returns a list of two plots.
+#' \itemize{
+#' \item mutationTypes - A pie chart showing the count and mutation types
+#' undergone in the sample
+#' \item substitutionTypes - A bar plot showing the count and substitution
+#' types undergone in the sample
+#' }
+#'
+#' @examples
+#' mutationTypePlotResults <- mutationTypePlot(icgc_data)
+#' #To display each plot
+#' mutationTypePlotResults["mutationTypes"]
+#' mutationTypePlotResults["substitutionTypes"]
+#'
+#' @references
+#' Hadley Wickham, Romain Francois, Lionel Henry and Kirill Muller
+#' (2022). dplyr: A Grammar of Data Manipulation. R package version
+#' 1.0.10. https://CRAN.R-project.org/package=dplyr
+#'
+#' H. Wickham. ggplot2: Elegant Graphics for Data Analysis. Springer-Verlag New
+#' York, 2016.
+#'
+#' Hadley Wickham (2022). stringr: Simple, Consistent Wrappers for Common
+#' String Operations. R package version 1.4.1.
+#' https://CRAN.R-project.org/package=stringr
+#'
+#' @export
+#' @import dplyr
+#' @importFrom ggplot2 ggplot
+#' @importFrom stringr str_extract
 
 mutationTypePlot <- function(mutationData){
-  #leave only unique mutation id's
-  inter <- dplyr::distinct(mutationData, mutationData$icgc_mutation_id,.keep_all = TRUE)
+
+  #leave only unique icgc_mutation_id
+  inter <- dplyr::distinct(mutationData,
+                           mutationData$icgc_mutation_id,.keep_all = TRUE)
+  #Delete newly created column from previous code
   inter <- inter[,-(length(inter))]
 
-  inter <- dplyr::filter(inter, inter$consequence_type == "stop_gained" | inter$consequence_type == "missense_variant"| inter$consequence_type == "
+  #Only filter for consequence_types that have cds code
+  inter <- dplyr::filter(inter, inter$consequence_type == "stop_gained" |
+  inter$consequence_type == "missense_variant"|
+  inter$consequence_type == "
 synonymous_variant")
-  inter <- dplyr::mutate(inter, substitution_type = stringr::str_extract(inter$cds_mutation, "[A-Z]>[A-Z]"))
+
+  #Use str_extract function to extract nucleotide change string from cds code
+  inter <- dplyr::mutate(inter, substitution_type = stringr::str_extract(
+    inter$cds_mutation, "[A-Z]>[A-Z]"))
+
+  #Get count of each string extracted from previous line
   inter <- dplyr::add_count(inter, count = substitution_type)
+
+  #Only need the substitution types now filter
   inter <- dplyr::distinct(inter, substitution_type, .keep_all = TRUE)
 
+  #Creating bar plot for substitution types
   bar_plot <- ggplot2::ggplot(inter, ggplot2::aes(x = substitution_type, y = n)) +
     ggplot2::geom_bar(stat = "identity", color = "black", fill ="white") +
     ggplot2::xlab("Substitution type") +
@@ -54,18 +148,23 @@ synonymous_variant")
     ggplot2::geom_text(ggplot2::aes(label=n), vjust=2, color="black")
 
 
+  #Formatting data into intermediate variable pie_inter to create pie chart
   pie_inter <- dplyr::add_count(mutationData, count = consequence_type)
   pie_inter <- dplyr::distinct(pie_inter, consequence_type, .keep_all = TRUE)
   pie_inter <- dplyr::mutate(pie_inter, concat_string = paste(consequence_type, n, sep = ", Count: "))
 
   #Pie chart for mutation types
-  pie_chart <- ggplot2::ggplot(pie_inter, ggplot2::aes(x = "", y = n,fill = concat_string)) +
+  pie_chart <- ggplot2::ggplot(pie_inter, ggplot2::aes(x = "",
+                                            y = n,fill = concat_string)) +
     ggplot2::geom_col(color = "black") +
     ggplot2::coord_polar(theta = "y") +
-    #ggplot2::scale_fill_discrete(labels = pie_inter$concat_string)
     ggplot2::labs(title = "Pie chart of Consequence type of mutation") +
     ggplot2::xlab("") +
     ggplot2::ylab("")
-  result = list(bar_plot, pie_chart)
+
+  #Combine pie chart and bar graph to be able to return on one object
+  result = list(mutationTypes = pie_chart, substitutionTypes = bar_plot)
+
+
   return(result)
 }
